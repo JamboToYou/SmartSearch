2c40adb75845524b03716cf205e409a76943ad32
Alexander Selyutin <cool.selutin99@yandex.ru>
2018-03-24 02:55:07 +0400

    Unparse

--- unparse.py
+++ unparse.py
@@ -2,14 +2,22 @@
 """
 Created on Fri Mar 23 20:45:24 2018
 
-@author: Александр
-"""
+@author: Александр"""
 
 import requests
 from bs4 import BeautifulSoup
-page = requests.get("http://www.google.ru/search?q=android&as_qdr=h5") #Запрос приводить к форме search?q=*
-soup = BeautifulSoup(page.content)
-import re
-links = soup.findAll("a")
-for link in  soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-    print(re.split(":(?=http)",link["href"].replace("/url?q=","")))
+
+def get_links(url):
+    
+    links = []
+    
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,"lxml")
+    
+    for item in soup.select(".r a"):
+        if("http" in str(item)):
+            links.append(str(item)[str(item).index('http'):str(item).index('&')])
+            
+    return links
+
+#print(get_links('https://www.google.ru/search?q=android'))
