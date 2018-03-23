12233f540c7dc197d44808b511c1ef8d72ea1030
Jambo <jambotoyou@gmail.com>
2018-03-24 01:32:32 +0400

    added tz for gocha

--- vk_module.py
+++ vk_module.py
@@ -5,3 +5,9 @@ vkApi = vk.API(sess)
 
 def send_message(user_id, message):
     vkApi.messages.send(user_id=user_id, message=message, v=5.73)
+
+"""
+обернуть в функции методы api а именно
+get_posts
+если что я скажу еще
+"""
