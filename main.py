63b1793e6f91b79acfc38dda76fb0fdd25162590
NesQ1k <111gosha11@gmail.com>
2018-03-24 10:52:55 +0400

    Views

--- main.py
+++ main.py
@@ -21,26 +21,63 @@ def create_view_for_views(view):
 
 ##############################
 
+def create_view_add_user(user_id):
+    r = execute("add_user",user_id=user_id)
+    if(r==1):
+        return "Добавление пользователя прошло успешно"
+    elif(r==0):
+        return "Ошибка добавления пользователя"
+
+def create_view_add_category(category_name):
+    r = execute("add_category", category_name=category_name)
+    if (r==1):
+        return "Добавление категории прошло успешно"
+    elif(r==0):
+        return "Ошибка добавления категории"
+
+def create_view_subscribe_to_category(category_id, user_id):
+    r = execute("subscribe_to_category", category_id=category_id, user_id=user_id)
+    if (r == 1):
+        return "Подписка на категорию прошла успешно"
+    elif (r == 0):
+        return "Ошибка подписки на категорию"
+
+def create_view_add_content(category_id, source):
+    r = execute("add_content", category_id=category_id, source=source)
+    if (r==1):
+        return "Добавление содержания прошло успешно"
+    elif(r==0):
+        return "Ошибка добавления содержания"
+
+def create_view_get_users():
+    view = ""
+    result = execute("get_users")
+    for cortege in result:
+        view += "{}. {}\n".format(str(cortege[0]), str(cortege[1]))
+    return view
+
 def create_view_get_categories():
     view = ""
     result = execute("get_categories")
     for cortege in result:
-        view+="{}. {}".format(str(cortege[0]), cortege[1])
+        view+="{}. {}\n".format(str(cortege[0]), str(cortege[1]))
     return view
 
 def create_view_content_of_category(category_id):
-    view = ""
-    result = execute("get_content_of_category",category_id=category_id)
-    for cortege in result:
-        view+="{}. {}".format(str(cortege[0]),cortege[1])
-    return view
+    view = {}
 
-def create_view_subscribe_to_category(category_id, user_id):
-    view = ""
-    result = execute("subscribe_to_category", category_id=category_id, user_id=user_id)
-    for cortege in result:
-        view += "{}. {}".format(str(cortege[0]), cortege[1], cortege[2])
-    return view
+    view_string = ""
+
+    result = execute("get_content_of_category",category_id=category_id)
+    keyword = execute("get_category_name_by_id", category_id=category_id)[0][0]
+    for content in result:
+        view = dict(view, **unparse.get_links(content[0], keyword))
+
+    for key,value in view.items():
+        view_string+="{}.\n {}\n".format(key,value)
+
+    return view_string
+
 
 ##############################
 
