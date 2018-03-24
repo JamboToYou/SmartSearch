440ffa7a64b02a4820b5b40ecf9d39f1d9b029bd
Alexander Selyutin <cool.selutin99@yandex.ru>
2018-03-24 08:47:41 +0400

    Unparse new

--- unparse.py
+++ unparse.py
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 23 20:45:24 2018
+Created on Sat Mar 24 08:43:02 2018
 
-@author: Александр"""
-
-import requests
-from bs4 import BeautifulSoup
-
-def get_links(site,keyword):
-    
-    url = "https://google.ru/search?q="+keyword+"&as_sitesearch="+site+"&as_qdr=d3"
-    print(url)
-    links = []
-    
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text,"lxml")
-    
-    for item in soup.select(".r a"):
-        if("http" in str(item)):
-            links.append(str(item)[str(item).index('http'):str(item).index('&')])
-            
-    return links
-
-#print(get_links('habrahabr.ru','android'))
+@author: Александр
+"""
