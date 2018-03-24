d68e133e17c03930118153bf3d851a04806b9097
Jambo <jambotoyou@gmail.com>
2018-03-24 09:05:46 +0400

    added few methods to dbmanager

--- dbmanager.py
+++ dbmanager.py
@@ -10,11 +10,45 @@ def add_category(c, category_name):
     result = c.execute("INSERT INTO Categories(category_name) VALUES(%s)", category_name)
     return result
 
+def subscribe_to_category(c, category_id, user_id):
+    result = c.execute("INSERT INTO CategoriesList(user_id, category_id) VALUES(%s, %s)", (user_id, category_id))
+    return result
+
+def add_content(c, category_id, source):
+    result = c.execute("INSERT INTO Content(source, category_id) VALUES(%s, %s)", (source, category_id))
+    return result
+
+def get_users(c):
+    c.execute("SELECT user_id FROM Users")
+    result = c.fetchall()
+    return result
+
+def get_categories(c):
+    c.execute("SELECT category_id, category_name FROM Categories")
+    result = c.fetchall()
+    return result
+
+def get_category_subscribers(c, category_id):
+    c.execute("SELECT user_id FROM CategoryList WHERE category_id = %s", category_id)
+    result = c.fetchall()
+    return result
+
+def get_content_of_category(c, category_id):
+    c.execute("SELECT source FROM Content WHERE category_id = %s", category_id)
+    result = c.fetchall()
+    return result
+
 
 methods = \
     {
         "add_user" : add_user,
-        "add_category" : add_category
+        "add_category" : add_category,
+        "subscribe_to_category" : subscribe_to_category,
+        "add_content" : add_content,
+        "get_users" : get_users,
+        "get_categories" : get_categories,
+        "get_category_subscribers" : get_category_subscribers,
+        "get_content_of_category" : get_content_of_category
     }
 
 def execute(method_name, **kwargs):
