9c4c3c0e2fb8905246c50c19554e81f68a6d5269
JamboToYou <jambotoyou@gmail.com>
2018-03-24 12:21:01 +0000

    something went wrong

--- unparse.py
+++ unparse.py
@@ -8,18 +8,18 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_links(site,keyword):
-    
-    url = "https://google.ru/search?q="+keyword+"&as_sitesearch="+site+"&as_qdr=d3"
+
+    url = "https://google.com/search?q="+keyword+"&as_sitesearch="+site+"&as_qdr=d3"
     print(url)
     links = {}
-    
+
     response = requests.get(url)
     soup = BeautifulSoup(response.text,"lxml")
-    
+
     for item in soup.select(".r a"):
         if("http" in str(item)):
             links[item.text] = str(item)[str(item).index('http'):str(item).index('&')]
-            
+
     return links
 
 #print(get_links('habrahabr.ru','android'))
