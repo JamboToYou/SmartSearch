968b904db8554658d67349723eb598dc3641d2dd
Alexander Selyutin <cool.selutin99@yandex.ru>
2018-03-25 08:15:20 +0400

    unparse

--- unparse.py
+++ unparse.py
@@ -4,15 +4,17 @@ from bs4 import BeautifulSoup
 
 def get_links(sites, keyword):
     links = {}
+    site_name = ""
     for site in sites:
-        url = "http://google.ru/search?q=" + keyword + "&as_sitesearch=" + site + "&as_qdr=d3"
+        site_name += "+site%3A" + site + "+%7C"
+    url = "http://google.ru/search?q=" + keyword + site_name + "&as_qdr=d3"
 
 
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "lxml")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
 
-        for item in soup.select(".r a"):
-            if ("http" in str(item)):
-                links[item.text] = str(item)[str(item).index('http'):str(item).index('&')]
+    for item in soup.select(".r a"):
+        if ("http" in str(item)):
+            links[item.text] = str(item)[str(item).index('http'):str(item).index('&')]
 
     return links
