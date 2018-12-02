c2d7b82be6d03c219e7f6ea2994afba97430e739
Андрей <31936190+JamboToYou@users.noreply.github.com>
2018-12-02 15:20:47 +0400

    Update testGosh.py

--- testGosh.py
+++ testGosh.py
@@ -1,7 +1,7 @@
 import vk
 
 #sess = vk.Session(access_token='17e584f856cadd64eb3aa7acb3537fdbcea60479ef87cd05f05123a34dbe8f41e344f9502def91c13d544')
-sess = vk.AuthSession(app_id=6389848, user_login='89913970602', user_password='Xs(f*84F2i1r9%n')
+sess = vk.AuthSession(app_id=123456, user_login='89012345678', user_password='************')
 vkApi = vk.API(sess)
 
 print(vkApi.wall.get(domain='emtech', v=5.73)['items'][0]['text'])
